c2d3d434b3e2917b865498f1e4c354f968576f6e
Om Tekade <omtekade914@gmail.com>
2024-01-25 22:55:37 +0530

    month and day total expense function add

--- app/(platform)/(dashboard)/group/[groupId]/balances/page.tsx
+++ app/(platform)/(dashboard)/group/[groupId]/balances/page.tsx
@@ -3,14 +3,20 @@ import { Info } from "../_components/info/info";
 import { BalanceList } from "./_components/balance-list";
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
-import { startOfMonth, endOfMonth } from "date-fns";
+import { startOfMonth, endOfMonth, startOfDay, endOfDay, format } from "date-fns";
 import { PercentDiamond } from "lucide-react";
 
 const BalancesPage = async () => {
   const { orgId, userId } = auth();
 
-  const startDate = startOfMonth(new Date());
-  const endDate = endOfMonth(new Date());
+  const today = new Date();
+  const startDate = startOfMonth(today);
+  const endDate = endOfMonth(today);
+  const startDay = startOfDay(today);
+  const endDay = endOfDay(today);
+  const Month = format(today, 'MMMM');
+  const todayDate = format(today, 'd MMM');
+  console.log(todayDate);
 
   if (!orgId || !userId) {
     return null;
@@ -29,7 +35,25 @@ const BalancesPage = async () => {
     },
   });
 
+  const todayTotalExpense = await db.expenseUser.findMany({
+    where:{
+      userId: userId,
+      createdAt: {
+        gte: startDay,
+        lte: endDay
+      },
+    },
+    select:{
+      amount : true
+    }
+  });
+
   const thisMonthTotalExpense = monthTotalExpense.reduce(
+    (sum, exp) => sum + parseFloat(exp.amount),
+    0
+  );
+
+  const todayTotal = todayTotalExpense.reduce(
     (sum, exp) => sum + parseFloat(exp.amount),
     0
   );
@@ -44,12 +68,26 @@ const BalancesPage = async () => {
           <BalanceList />
         </div>
         <div className="flex-1">
-          <div className="flex gap-2 px-2 md:px-4 text-md sm:text-lg">
-            <div className="flex items-center font-semibold text-neutral-700">
-              <PercentDiamond className="h-6 w-6 mr-2" />
-              This month&apos;s spending :
+          <div className="flex flex-col gap-4 px-2 md:px-4 text-md sm:text-lg">
+            <div className="flex gap-2">
+              <div className="flex items-center font-semibold text-neutral-700">
+                <PercentDiamond className="h-6 w-6 mr-2" />
+                {todayDate}&apos;s total expense :
+              </div>
+              <div className="ml-auto">
+                <h1 className="font-semibold">{todayTotal} ₹</h1>
+              </div>
             </div>
-            <h1 className="font-semibold">{thisMonthTotalExpense} ₹</h1>
+            <Separator />
+            <div className="flex gap-2">
+              <div className="flex items-center font-semibold text-neutral-700">
+                <PercentDiamond className="h-6 w-6 mr-2" />
+                {Month}&apos;s total expense :
+              </div>
+              <div className="ml-auto">
+                <h1 className="font-semibold">{thisMonthTotalExpense} ₹</h1>
+              </div>
+            </div>
           </div>
         </div>
       </div>
